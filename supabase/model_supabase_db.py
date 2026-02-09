3a8ce173a928f94f8baff82b53c3b64a36968df9
Lucas <lucasalves_taz@hotmail.com>
2026-02-09 17:38:16 -0300

    up supabase

--- supabase/model_supabase_db.py
+++ supabase/model_supabase_db.py
@@ -1,11 +1,17 @@
+# Script com o modelo das tabelas
+
+# Bibliotecas utilizadas
 from sqlalchemy import Column, Date, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from .conect_supabase_db import Base
+
+# Definição das classes que representam as tabelas do banco de dados
 
 
 class clientes(Base):
     __tablename__ = "clientes"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(255), nullable=False)
     sobrenome = Column(String(255), nullable=False)
@@ -22,6 +28,7 @@ class clientes(Base):
     data_cadastro = Column(Date, nullable=False)
     id_emailmarketing = Column(Integer, ForeignKey("emailmarketing.id"), nullable=False)
 
+    # Relacionamentos
     pedidos = relationship("pedidos", back_populates="cliente")
     generocliente = relationship("generocliente", back_populates="cliente")
     estadocivil = relationship("estadocivil", back_populates="cliente")
@@ -31,6 +38,7 @@ class clientes(Base):
 class produtos(Base):
     __tablename__ = "produtos"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     sku = Column(String(50), nullable=False, unique=True)
     nome = Column(String(255), nullable=False)
@@ -46,6 +54,7 @@ class produtos(Base):
     id_genero = Column(Integer, ForeignKey("generoproduto.id"), nullable=False)
     cor = Column(String(20))
 
+    # Relacionamentos
     pedidos = relationship("pedidos", back_populates="produto")
     itens = relationship("itens_pedido", back_populates="produto")
     categoria = relationship("categorias", back_populates="produto")
@@ -56,6 +65,7 @@ class produtos(Base):
 class pedidos(Base):
     __tablename__ = "pedidos"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     id_cliente = Column(Integer, ForeignKey("clientes.id"), nullable=False)
     id_produto = Column(Integer, ForeignKey("produtos.id"), nullable=False)
@@ -77,6 +87,7 @@ class pedidos(Base):
     data_entrega = Column(Date)
     id_entregue = Column(Integer, ForeignKey("entregue.id"), nullable=False)
 
+    # Relacionamentos
     cliente = relationship("clientes", back_populates="pedidos")
     produto = relationship("produtos", back_populates="pedidos")
     itens = relationship("itens_pedido", back_populates="pedido")
@@ -89,96 +100,117 @@ class pedidos(Base):
 class status(Base):
     __tablename__ = "status"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False, unique=True)
 
+    # Relacionamentos
     pedido = relationship("pedidos", back_populates="status")
 
 
 class formapagamento(Base):
     __tablename__ = "formapagamento"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False, unique=True)
 
+    # Relacionamentos
     pedido = relationship("pedidos", back_populates="formapagamento")
 
 
 class canalvenda(Base):
     __tablename__ = "canalvenda"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False, unique=True)
 
+    # Relacionamentos
     pedido = relationship("pedidos", back_populates="canalvenda")
 
 
 class generocliente(Base):
     __tablename__ = "generocliente"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(20), nullable=False, unique=True)
 
+    # Relacionamentos
     cliente = relationship("clientes", back_populates="generocliente")
 
 
 class generoproduto(Base):
     __tablename__ = "generoproduto"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(20), nullable=False, unique=True)
 
+    # Relacionamentos
     produto = relationship("produtos", back_populates="generoproduto")
 
 
 class categorias(Base):
     __tablename__ = "categorias"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False, unique=True)
 
+    # Relacionamentos
     produto = relationship("produtos", back_populates="categoria")
 
 
 class marcas(Base):
     __tablename__ = "marcas"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(50), nullable=False, unique=True)
 
+    # Relacionamentos
     produto = relationship("produtos", back_populates="marca")
 
 
 class estadocivil(Base):
     __tablename__ = "estadocivil"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(20), nullable=False, unique=True)
 
+    # Relacionamentos
     cliente = relationship("clientes", back_populates="estadocivil")
 
 
 class emailmarketing(Base):
     __tablename__ = "emailmarketing"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(255), nullable=False, unique=True)
 
+    # Relacionamentos
     cliente = relationship("clientes", back_populates="emailmarketing")
 
 
 class entregue(Base):
     __tablename__ = "entregue"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     nome = Column(String(255), nullable=False, unique=True)
 
+    # Relacionamentos
     pedido = relationship("pedidos", back_populates="entregue")
 
 
 class itenspedido(Base):
     __tablename__ = "itenspedido"
 
+    # Campos da tabela
     id = Column(Integer, primary_key=True)
     id_pedido = Column(Integer, ForeignKey("pedidos.id"), nullable=False)
     id_produto = Column(Integer, ForeignKey("produtos.id"), nullable=False)
@@ -186,5 +218,6 @@ class itenspedido(Base):
     preco_unitario = Column(Float, nullable=False)
     subtotal = Column(Float, nullable=False)
 
+    # Relacionamentos
     pedido = relationship("pedidos", back_populates="itens")
     produto = relationship("produtos", back_populates="itens")
